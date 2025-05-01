0bd99fde322eb72e1cc443ea749c946e42c46412
Cristina Lee <cristinalee0723@gmail.com>
2025-05-01 16:09:05 -0400

    Implement Event Routes
    - Added Frequency Enum for more consistency

--- backend/src/db.py
+++ backend/src/db.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import enum
 
 db = SQLAlchemy()
 
@@ -9,6 +10,12 @@ task_category_assc = db.Table("task_category_assc", db.Model.metadata, db.Column
 
 # Classes
 
+class Frequency(enum.Enum):
+    DAILY = "DAILY"
+    WEEKLY = "WEEKLY"
+    MONTHLY = "MONTHLY"
+    NONE = "NONE"  # for one-time events
+    
 class Event(db.Model):
     """
     Event Model 
@@ -18,11 +25,10 @@ class Event(db.Model):
 
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     title = db.Column(db.String, nullable = False)
-    recurrence = db.Column(db.String, nullable = True)
-    day_of_week = db.Column(db.String, nullable = False)
+    recurrence = db.Column(db.Enum(Frequency), default=Frequency.NONE, nullable=False)
     start_time_frame = db.Column(db.String, nullable = False)
     end_time_frame = db.Column(db.String, nullable = False)
-    timespan = db.relationship("Timespan", cascade = "delete")
+    timespan = db.relationship("Timespan", cascade = "all, delete", back_populates="event")
     category = db.relationship("Category", secondary = event_category_assc, back_populates = "events")
 
 
@@ -32,7 +38,6 @@ class Event(db.Model):
         """
         self.title = kwargs.get("title", "")
         self.recurrence = kwargs.get("recurrence", "")
-        self.day_of_week = kwargs.get("day_of_week", "")
         self.start_time_frame = kwargs.get("start_time_frame", "")
         self.end_time_frame = kwargs.get("end_time_frame", "")
     
@@ -45,7 +50,6 @@ class Event(db.Model):
             "id": self.id,
             "title": self.title,
             "recurrence": self.recurrence,
-            "day_of_week": self.day_of_week,
             "start_time_frame": self.start_time_frame,
             "end_time_frame": self.end_time_frame,
             "timespan" : [t.serialize() for t in self.timespan],            
@@ -59,14 +63,13 @@ class Event(db.Model):
         return {
             "id": self.id,
             "title": self.title,
-            "recurrence": self.recurrence,
-            "day_of_week": self.day_of_week,
+            "recurrence": self.recurrence.value,
             "start_time_frame": self.start_time_frame,
             "end_time_frame": self.end_time_frame,
-            "timespan" : [t.serialize() for t in self.timespan],            
-            "category" : [c.serialize() for c in self.category]
+            "timespan" : [t.simple_serialize() for t in self.timespan],            
+            "category" : [c.simple_serialize() for c in self.category]
         }
-
+        
 
 class Category(db.Model):
     """
@@ -86,6 +89,15 @@ class Category(db.Model):
         Initialize Category object/entry
         """
         self.category = kwargs.get("category", "")
+
+    def simple_serialize(self):
+        """
+        Serialize a Category object
+        """
+        return {
+            "id": self.id,
+            "category": self.category
+        }
     
     def serialize(self):
         """
@@ -109,9 +121,9 @@ class Timespan(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     start_time = db.Column(db.String, nullable = False)
     end_time = db.Column(db.String, nullable = False)
-    date = db.Column(db.String, nullable = False)
+    day_of_week = db.Column(db.String, nullable=False)  # e.g. "MON", "TUE"
     event_id = db.Column(db.Integer, db.ForeignKey("events.id"), nullable = False)
-
+    event = db.relationship("Event", back_populates="timespan")
 
     def __init__ (self, **kwargs):
         """
@@ -119,9 +131,19 @@ class Timespan(db.Model):
         """
         self.start_time = kwargs.get("start_time", "")
         self.end_time = kwargs.get("end_time", "")
-        self.date = kwargs.get("date", "")
+        self.day_of_week = kwargs.get("day_of_week", "")
         self.event_id = kwargs.get("event_id")
 
+    def simple_serialize(self):
+            """
+            Serialize a Timespan object
+            """
+            return {
+                "id": self.id,
+                "start_time": self.start_time,
+                "end_time" : self.end_time,
+                "day_of_week": self.day_of_week,
+            }
 
     def serialize(self):
         """
@@ -131,7 +153,7 @@ class Timespan(db.Model):
             "id": self.id,
             "start_time": self.start_time,
             "end_time" : self.end_time,
-            "date": self.date,
+            "day_of_week": self.day_of_week,
             "event_id" : self.event_id
         }
     
